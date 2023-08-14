8fef308bf8ac1e80b104f3485708d24d87bee54a
Daniel-0117 <nevaslon0117@gmail.com>
2023-08-13 19:59:06 -0700

    Final changes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -10,7 +10,12 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category }, { model: Tag }],
+      include:{ 
+        model: Category,
+        attributes: ['id', 'category_name'],
+        model: Tag,
+        attributes: ['id', 'tag_name'], 
+      },
     });
     res.status(200).json(productData);
   }
@@ -25,7 +30,12 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: Tag }],
+      include:{ 
+        model: Category,
+        attributes: ['id', 'category_name'],
+        model: Tag,
+        attributes: ['id', 'tag_name'], 
+      },
     });
     if (!productData) {
       res.status(404).json({ message: 'No product found with that id!' });
@@ -39,38 +49,17 @@ router.get('/:id', async (req, res) => {
 
 });
 
-// create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-});
 
+// create new product
+    router.post('/', async (req, res) => {
+      try {
+        const productData = await Product.create(req.body);
+        res.status(200).json(productData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
+    
 // update product
 router.put('/:id', (req, res) => {
   // update product data
